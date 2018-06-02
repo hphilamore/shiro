cb2f08e1ce70d6710a04efebf0995ab40e7e67f8
hphilamore <hemmaphilamore@gmail.com>
2018-06-02 12:00:49 +0900

    tracking multiple centres

--- ball_tracking_example.py
+++ ball_tracking_example.py
@@ -77,34 +77,35 @@ while True:
 		# it to compute the minimum enclosing circle and
 		# centroid
 		print(len(cnts))
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		#print(center)
+		for c in cnts:
+		#c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			#print(center)
 
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+			# only proceed if the radius meets a minimum size
+			if radius > 10:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(x), int(y)), int(radius),
+					(0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
 	# update the points queue
-	pts.appendleft(center)
-
-	# loop over the set of tracked points
-	for i in range(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
-
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+	# pts.appendleft(center)
+    #
+	# # loop over the set of tracked points
+	# for i in range(1, len(pts)):
+	# 	# if either of the tracked points are None, ignore
+	# 	# them
+	# 	if pts[i - 1] is None or pts[i] is None:
+	# 		continue
+    #
+	# 	# otherwise, compute the thickness of the line and
+	# 	# draw the connecting lines
+	# 	thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+	# 	cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
